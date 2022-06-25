a3a220cf9ff324c5d262e47d1ca6409f6e884062
Febrianto <febrianto.bekasi@gmail.com>
2022-06-25 16:39:42 +0700

    create admin model

--- administrator/models.py
+++ administrator/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Admin(models.Model):
+  nama = models.CharField(max_length=40)
+  username = models.CharField(max_length=40, unique=True)
+  password = models.CharField(max_length=100)
+  email = models.EmailField(max_length=50)
+  no_telp = models.CharField(max_length=14)
+  
+  def save(self):
+    return f'{self.id}. {self.username}'
